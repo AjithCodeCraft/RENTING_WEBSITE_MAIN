130fc569da04e39eb6780715c50a17af3cc1885e
alameen27 <alameena068@gmail.com>
2025-02-11 22:41:24 +0530

    apartment api and accesstoken set to 1 hour

--- backend/Rental_project/rental_app/urls.py
+++ backend/Rental_project/rental_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import add_house_owner, register_user, login_user
+from .views import add_house_owner, register_user, login_user,apartment_list_create, apartment_detail
 
 urlpatterns = [
     path('signup/', register_user, name='register_user'),  # 🔹 User Registration
     path('login/', login_user, name='login_user'),
     path('add-house-owner/', add_house_owner, name='add-house-owner'),
+    path('apartments/', apartment_list_create, name='apartment-list-create'),  # List & Create
+    path('apartments/<uuid:pk>/', apartment_detail, name='apartment-detail'),
 ]
