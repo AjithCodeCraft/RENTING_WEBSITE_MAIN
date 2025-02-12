de4c166b0c34e26dd57cd0807ee7bc1025d539ad
alameen27 <alameena068@gmail.com>
2025-02-12 11:35:31 +0530

    hashed pass and some apis created

--- backend/Rental_project/rental_app/urls.py
+++ backend/Rental_project/rental_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import add_house_owner, register_user, login_user,apartment_list_create, apartment_detail
+from .views import add_house_owner, get_apartments_by_owner, get_house_owner_by_id, get_house_owner_by_ssn, register_user, login_user,apartment_list_create, apartment_detail
 
 urlpatterns = [
     path('signup/', register_user, name='register_user'),  # 🔹 User Registration
@@ -7,4 +7,7 @@ urlpatterns = [
     path('add-house-owner/', add_house_owner, name='add-house-owner'),
     path('apartments/', apartment_list_create, name='apartment-list-create'),  # List & Create
     path('apartments/<uuid:pk>/', apartment_detail, name='apartment-detail'),
+    path('api/house-owner/by-id/<str:owner_id>/', get_house_owner_by_id, name='get-house-owner-by-id'),
+    path('api/house-owner/by-ssn/<str:ssn>/', get_house_owner_by_ssn, name='get-house-owner-by-ssn'),
+    path('api/apartment/by-owner/<str:owner_id>/', get_apartments_by_owner, name='get_apartments_by_owner'),
 ]
